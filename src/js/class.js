10fa7c88a4329c4abf1b7737b2907ade39046d51
YablonskaTanya <111698588+YablonskaTanya@users.noreply.github.com>
2023-03-11 22:28:27 +0200

    add practice

--- src/js/class.js
+++ src/js/class.js
@@ -186,3 +186,20 @@ class Car {
 // console.log(mango);
 
 //
+
+function XO(str) {
+  const x = [];
+  const o = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i].toLowerCase() === 'x') {
+      x.push(str[i]);
+    } else if (str[i].toLowerCase() === 'o') {
+      o.push(str[i]);
+    }
+  }
+
+  if (x.length === o.length) {
+    return true;
+  } else false;
+}
